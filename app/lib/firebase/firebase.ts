c7cd8990153dc353c386b3560d09a9561d79ad32
theo <townhelpemail@gmail.com>
2024-04-08 13:01:41 -0400

    Update firebase.ts

--- app/lib/firebase/firebase.ts
+++ app/lib/firebase/firebase.ts
@@ -9,7 +9,7 @@ export const firebaseApp = {
   storageBucket: "streamstarnode.appspot.com",
   messagingSenderId: "563639367279",
   appId: "1:563639367279:web:b30145b5e176fe01b17f9a",
-  measurementId: "G-J6Z0Q25N8N",
+  measurementId: "G-J6Z0Q25N8N"
 };
 
 export const app = initializeApp(firebaseApp);
